6b33966e498ae1cc39e0e84c8f99f078bbf91177
Daniele Nicolodi <daniele@grinta.net>
2025-06-08 22:50:40 +0200

    BUG: allow $MACOSX_DEPLOYMENT_TARGET to be set to major version only
    Accept version specifiers containing only the major version, for
    example "11", in addition to the already supported major.minor and
    major.minor.patch formats, for example "11.0" or "11.0.0".
    
    - meson-python commit cd4421478f7c112e85c7feacb5034bb80d653222

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -61,6 +61,17 @@ def test_macos_platform_tag(monkeypatch):
                 next(packaging.tags.mac_platforms((major, minor)))
                 == hatch_meson._tags.get_platform_tag()
             )
+    for major in range(11, 13):
+        monkeypatch.setenv("MACOSX_DEPLOYMENT_TARGET", f"{major}.0")
+        assert (
+            next(packaging.tags.mac_platforms((major, 0)))
+            == hatch_meson._tags.get_platform_tag()
+        )
+        monkeypatch.setenv("MACOSX_DEPLOYMENT_TARGET", f"{major}")
+        assert (
+            next(packaging.tags.mac_platforms((major, 0)))
+            == hatch_meson._tags.get_platform_tag()
+        )
 
 
 @pytest.mark.skipif(sys.platform != "darwin", reason="macOS specific test")
